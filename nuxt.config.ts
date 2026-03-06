a22a1defa24af143197dfa0e0128dc45f88b6bed
TDESIGN96 <tayseerip@gmail.com>
2026-03-06 08:44:33 +0300

    big update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineNuxtConfig({future: {
+  compatibilityVersion: 4  // ← enables Nuxt 4 app/ structure
+},
+  modules: ['shadcn-nuxt'],
+  
+  shadcn: {
+    /**
+     * Prefix for all the imported component.
+     * @default "Ui"
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * Will respect the Nuxt aliases.
+     * @link https://nuxt.com/docs/api/nuxt-config#alias
+     * @default "@/components/ui"
+     */
+    componentDir: '@/components/ui'
+  },
+  css: ['~/assets/css/tailwind.css'],
+  vite: {
+    plugins: [tailwindcss() as any],
+  },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
 })
